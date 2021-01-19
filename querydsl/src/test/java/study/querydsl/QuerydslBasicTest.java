5c41a1a23c20652c113e5623202b6414bb935770
hyun6ik <fkanwm@naver.com>
2021-01-19 22:14:13 +0900

    Q-Type 활용

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -14,6 +14,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import static org.assertj.core.api.Assertions.*;
+import static study.querydsl.entity.QMember.*;
 
 @SpringBootTest
 @Transactional
@@ -56,14 +57,10 @@ public class QuerydslBasicTest {
     @Test
     public void startQuerydsl() throws Exception {
         //given
-
-        //when
-        QMember m = new QMember("m");
-
         Member findMember = queryFactory
-                .select(m)
-                .from(m)
-                .where(m.username.eq("member1"))  //파라미터 바인딩 처리
+                .select(member)
+                .from(member)
+                .where(member.username.eq("member1"))  //파라미터 바인딩 처리
                 .fetchOne();
         //then
         assertThat(findMember.getUsername()).isEqualTo("member1");
