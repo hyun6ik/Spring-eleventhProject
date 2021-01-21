0f51f5075ce22d6836db803acfc71e6089dbed17
hyun6ik <fkanwm@naver.com>
2021-01-22 03:52:29 +0900

    동적 쿼리와 성능 최적화 조회 - Builder사용

---
+++ querydsl/src/main/java/study/querydsl/dto/MemberTeamDto.java
@@ -0,0 +1,25 @@
+package study.querydsl.dto;
+
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class MemberTeamDto {
+
+    private Long memberId;
+    private String username;
+    private int age;
+    private Long teamId;
+    private String teamname;
+
+    @QueryProjection
+    public MemberTeamDto(Long memberId, String username, int age, Long teamId, String teamname) {
+        this.memberId = memberId;
+        this.username = username;
+        this.age = age;
+        this.teamId = teamId;
+        this.teamname = teamname;
+    }
+}
